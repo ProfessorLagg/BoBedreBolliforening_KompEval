2f79c2bf76f779a7c42ff3ea0df1c96caa41f197
ProfessorLagg <93381546+ProfessorLagg@users.noreply.github.com>
2022-04-23 18:11:53 +0200

    Finished up the tables and Added Data

---
+++ BoBedre/AddData.sql
@@ -0,0 +1,37 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+INSERT INTO Vaskerier(Navn,Aaben,Luk)
+VALUES
+('Whitewash Inc.','08:00','20:00'),
+('Double Bubble','02:00','22:00'),
+('Double Bubble','12:00','20:00');
+
+GO
+
+INSERT INTO Brugere (Navn,EMAIL,Pass,Konto,Vaskeri)
+VALUES
+('John','john_doe66@gmail.com','password',100.00,2),
+('Niel Armstrong','firstman@nasa.gov','eagleLander69',1000.00,2),
+('Batman','noreply@thecave.com','Rob1nIsCool',100.00,2),
+('Goldman Sachs','moneylaundering@gs.com','NotRecognized',100.00,2),
+('50 Cent','50cent@gmail.com','ItsMyBirthday',100.00,2);
+
+GO
+
+INSERT INTO Maskiner(Navn,Pris,VaskeTid,Vaskeri)
+VALUES
+('Mielle 911 Turbo',5.00,'01:00:00',2),
+('Siemons IClean',10000.00,'00:30:00',1),
+('Electrolax FX-2',15.00,'00:45:00',2),
+('NASA Spacewasher 8000',500.00,'00:05:00',1),
+('The Lost Sock',3.50,'01:30:00',3),
+('Yo Mama',0.50,'02:00:00',3);
--- BoBedre/Brugere.sql
+++ BoBedre/Brugere.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Brugere]
 (
 	ID int IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	Navn Nvarchar NOT NULL,
-	EMAIL Nvarchar NOT NULL UNIQUE,
-	Pass Nvarchar NOT NULL, 
+	Navn Nvarchar(50) NOT NULL,
+	EMAIL Nvarchar(50) NOT NULL UNIQUE,
+	Pass Nvarchar(50) NOT NULL, 
 	Konto DECIMAL(19,4) DEFAULT 0 NOT NULL,
 	Vaskeri int FOREIGN KEY REFERENCES Vaskerier(ID) NOT NULL,
 	Oprettet datetime DEFAULT GetDate() NOT NULL,
